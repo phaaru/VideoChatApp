3150781db70696694907a768d9445b3a6691b91c
phaaru <1995gupta@gmail.com>
2020-04-21 05:52:49 +0530

    credentials

--- credentials.js
+++ credentials.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 module.exports = {
     // key: null,
     // cert: null
-    key: fs.readFileSync('ssl-cert-snakeoil-key.pem', 'utf8'),
-    cert: fs.readFileSync('ssl-cert-snakeoil.pem', 'utf8')
+    key: fs.readFileSync('./credentials/ssl-cert-snakeoil-key.pem', 'utf8'),
+    cert: fs.readFileSync('./credentials/ssl-cert-snakeoil.pem', 'utf8')
   };

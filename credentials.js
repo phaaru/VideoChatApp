5f4f0ebe5f7e2980f3809a9c4351a98c6e6b59a4
phaaru <1995gupta@gmail.com>
2020-04-21 18:29:48 +0530

    adding ec2's self-signed open ssl cert and key pem files

--- credentials.js
+++ credentials.js
@@ -1,11 +1,14 @@
-// const fs = require('fs');
+const fs = require('fs');
 module.exports = {
-    key: null,
-    cert: null
+    // key: null,
+    // cert: null
 
     // key: fs.readFileSync('./credentials/ssl-cert-snakeoil-key.pem', 'utf8'),
     // cert: fs.readFileSync('./credentials/ssl-cert-snakeoil.pem', 'utf8')
 
     // key: fs.readFileSync('./credentials/key.pem', 'utf8'),
     // cert: fs.readFileSync('./credentials/cert.pem', 'utf8')
+
+    key: fs.readFileSync('./credentials/RootCA-key.pem', 'utf8'),
+    cert: fs.readFileSync('./credentials/RootCA.pem', 'utf8')
   };

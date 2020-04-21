34045dea6e8c5887e6c457d9adbebdd9f012ddc9
phaaru <1995gupta@gmail.com>
2020-04-21 20:02:03 +0530

    adding 443 port

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const credentials = require('./credentials');
 const express = require('express');
 const app = express();
-// if (credentials.key && credentials.cert) {
-//   const https = require('https');
-//   server = https.createServer(credentials, app);
-//   port = 443;
-// } else {
-
+if (credentials.key && credentials.cert) {
+  const https = require('https');
+  server = https.createServer(credentials, app);
+  port = 443;
+} else {
 const http = require('http');
 server = http.createServer(app);
 port = 3000;
-
+}
 const io = require('socket.io')(server)
 
 const engine = require('./public/game')
